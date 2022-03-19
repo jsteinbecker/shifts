4dac9c100585a8646dbfc40e3cc63744514ee6c2
jsteinbecker <joshsteinbecker@icloud.com>
2022-03-19 02:58:43 -0600

    edit

--- epl.py
+++ epl.py
@@ -37,14 +37,17 @@ class empData :
       dic = dict(zip(self.shifts,cts))
       df = pd.Series(dic)
       return df
-         
+
+      
 class schedule :
       
-   def __init__(self,empData,start) :
+   def __init__(self,start,empData) :
       self.startdate = start
       self.length = 42
       blank = ["Day_" + str(x) for x in range((42))]
       self.df = pd.DataFrame(index=empData.df.name, columns=blank)
+      self.empdf = empData.df
+      self.emps = list(self.empdf['name'])
       
    def ptoInput (self) :
       ptoraw = open("pto_requests.yaml","r").read()
@@ -57,55 +60,90 @@ class schedule :
       return request
    
    def addPTO (self) :
+      """Access PTO Requests and Apply To Schedule
+      ============================================"""
       for emp,day in self.ptoInput():
          d = "Day_" + day
          self.df[d][emp] = "PTO"
       return self.df
    
    def getEmployeeWeek (self,weekn,emp) :
+      """Display Employee Schedule for specified week
+      ==============================================="""
       day0 = 0 + weekn * 7
       day6 = weekn * 7 + 7
       return self.df.loc[emp].iloc[day0:day6]
    
    def setWeekendsOff (self):
+      """Modify DF to give employees appropriate weekends off
+      ======================================================="""
+      # Generate column names of every Saturday and Sunday
       dayids = [x for x in range(42) if x%7 == 0 or x%7 == 6]
-      #Generate column names of every Saturday and Sunday
       daycols = ["Day_"+ str(x) for x in dayids]
-      #Generate names of employees who get all weekends off
+      # Generate names of employees who get all weekends off
       e = []
-      for x in emps.df.index:
-         if emps.df['weekend'].loc[x] == 0:
+      for x in self.empdf.index:
+         if self.empdf['weekend'].loc[x] == 0:
             e.append(x)
+      # Scratch out avaliability of empolyees who work 0 weekends
       for emp in e :
          for day in daycols:
             self.df[day].loc[emp] = "___"
       return self.df
    
    def exclusiveShifts (self):
-      emps = []
-      for x in emps.df:
-         if len(emps.df['trained_for'].loc[x]) == 1:
-            emps.append(x)
-      return emps
+      """Get list of employees who work 1 shift
+      ========================================="""
+      es = []
+      for x in self.emps:
+         if len(self.empdf['trained_for'].loc[x]) == 1:
+            es.append(x)
+      return es
+   
+   def employeeshiftsPerWeek (self,emp,viewmode=0):
+      """"""
+      shifts = list(self.df.loc[emp])
+      hoursperweek = [[] for x in range(int(42/7))]
+      for i in range(len(shifts)):
+         hoursperweek[i//7].append(shifts[i])
+      if viewmode == 1:
+         b = """"""
+         for x in hoursperweek:
+            b += """
+            """
+            b += str(x)
+         hoursperweek = b
+      return hoursperweek
+#
+#
+#
+def execute ():
+   
+   emps = empData()
+   schedule1 = schedule (6,emps)
+   emps.countWhoWorksShift()
+   sch_after_addPTO = schedule1.addPTO()
+   josh_week0 = schedule1.getEmployeeWeek(0,"Josh")
+   thelassa_week1 = schedule1.getEmployeeWeek(1,"Thelassa")
+   schedule1.df.loc['Thelassa']
+   schedule1.setWeekendsOff()
+   exc = schedule1.exclusiveShifts()
+   sch_emps = schedule1.emps
+   joshsShifts = schedule1.employeeshiftsPerWeek("Josh",1)
+   ts = schedule1.employeeshiftsPerWeek("Thelassa",viewmode=1)
+   
+   return {"EMPS OBJECT": emps, 
+           "EMPS IN SCHEDULE OBJECT": schedule1.empdf,
+           "SCHEDULE 01": schedule1.df, 
+           "JOSH -- WEEK 0":josh_week0,
+           "THELASSA -- WEEK 1": thelassa_week1,
+           "SCHEDULE EMPLOYEES": sch_emps,
+           "EXCULSIVE-SHIFT WORKERS (first to fill in scheduling their shift" : exc,
+           "JOSH'S SHIFTS":joshsShifts,
+           "THELASSA's SHIFTS": ts}
 
-dayids = [x for x in range(42) if x%7 == 0 or x%7 == 6]
-daycols = ["Day_"+ str(x) for x in dayids]
-daycols
-0%7
-6%7
-      
-emps = empData()
-len(emps.df['trained_for'].loc["Josh"])
-emps.countWhoWorksShift()
-
-schedule1 = schedule (emps,6)
-schedule1.df
-emps.df['weekend'].loc['Thelassa'] == 0
-schedule1.addPTO()
-schedule1.getEmployeeWeek(0,"Josh")
-
-
-schedule1.df.loc['Thelassa']
-
-schedule1.setWeekendsOff()
-schedule1.exclusiveShifts()
+execution = execute()
+for x in list(execution.keys()):
+   print(x)
+for x,y in execution.items():
+   print(f'{x}:\n{(len(x)+1 )*"-"}\n{y}\n\n\n')
